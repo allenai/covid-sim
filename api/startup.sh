d901fd49fcb11d556223af012160841978f4dc13
Michal Guerquin <michalg@allenai.org>
2020-06-26 16:13:43 -0700

    try using the large set of files

--- api/startup.sh
+++ api/startup.sh
@@ -3,6 +3,6 @@
 # Dataset files are expected to be available at this /skiff_files location.
 # But, the application reads files from /usr/src/app/covid-ai2/data so this
 # symlink lets it find the files it needs.
-ln -s /skiff_files/apps/covid-sim/demo_data /usr/src/app/covid-ai2/data
+ln -s /skiff_files/apps/covid-sim/demo_data_01 /usr/src/app/covid-ai2/data
 
 streamlit run demo.py --server.enableCORS false
