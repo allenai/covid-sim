f36beddd1962ff03091dc5936ed7e479b8a2d5f1
Shauli Ravfogel <shauli321@gmail.com>
2020-06-28 15:22:55 +0300

    Update startup.sh

--- api/startup.sh
+++ api/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DATASET_NAME="${DATASET_NAME:-data}"
-DATASET_URL="${DATASET_URL:-https://storage.googleapis.com/skiff-models/covid-sim/demo-data-partial/demo_data.zip}"
+DATASET_URL="${DATASET_URL:-https://storage.googleapis.com/skiff-models/covid-sim/demo-data-partial/demo_data_01.zip}"
 LOCAL_DATASET_DIR=/usr/src/app/covid-ai2
 
 echo "INFO: Downloading dataset from ${DATASET_URL})..."
 wget ${DATASET_URL} || exit 1
-unzip demo_data.zip -d ${LOCAL_DATASET_DIR}/${DATASET_NAME}
+unzip demo_data_01.zip -d ${LOCAL_DATASET_DIR}/${DATASET_NAME}
 
 streamlit run demo.py --server.enableCORS false
