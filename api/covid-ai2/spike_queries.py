d44cea933d46796e660d71606d2d3c6837e156e1
shauli <shauli321@gmail.com>
2020-10-04 15:59:36 +0300

    adding spike-based filtration

--- api/covid-ai2/spike_queries.py
+++ api/covid-ai2/spike_queries.py
@@ -26,6 +26,10 @@ def perform_query(query: str, dataset_name: str = "pubmed", num_results: int = 1
   "data_set_name": "{dataset_name}"
 }}"""
 
+    template = """{{
+      "queries": {{"{query_type}": "{query_content}"}},
+      "data_set_name": "{dataset_name}"
+    }}"""
 
 
     query = template.format(query_content=query, dataset_name=dataset_name, query_type=query_type,
