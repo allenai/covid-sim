6fc6f6acc8d135df176c1407a9f2296c1ac37020
shauli <shauli321@gmail.com>
2020-10-05 10:21:22 +0300

    adding spike-based filtration

--- api/covid-ai2/demo.py
+++ api/covid-ai2/demo.py
@@ -228,20 +228,22 @@ if start:
                             results_sents_filtered = []
 
                             def remove_all_words(s):
-                                words_to_remove = [" is", " are ", " the ", " a ", " an ", " to ", " as ", " from ",
+                                words_to_remove = [" is ", " are ", " the ", " a ", " an ", " to ", " as ", " from ",
                                                    " and ", " or ", " of ", " in ", " be ", " this ", " that ", " , ", " these ", " those ",
                                                    " with ", " within ", " can "]
                                 for w in words_to_remove:
                                     s = s.replace(w, " ")
+                                while "  " in s:
+                                    s = s.replace("  ", " ")
 
                                 s = s.replace("The ", "").replace("In ", "").replace("Although ", "").replace("It ", "").replace(" (", "").replace(" )", " ").replace("A ", "").replace("An ", "")
-                                return s
+                                return s.replace("  ", " ")
 
-                            all_words = " OR ".join(["("+ " AND ".join(remove_all_words(s).split(" ")[:5])+")" for s in result_sents][:10])
+                            all_words = " OR ".join(["("+ " AND ".join(remove_all_words(s).split(" ")[:5])+")" for s in result_sents][:])
                             all_words = all_words.replace("AND AND", "AND")
-                            st.write("TEST {}" + "AND AND" in all_words)
-                            st.write(all_words)
-                            st.write("######################################3")
+                            #st.write("TEST {}" + "AND AND" in all_words)
+                            #st.write(all_words)
+                            #st.write("######################################3")
                             results_df_filtration = spike_queries.perform_query(filter_query, dataset_name="covid19",
                                                                       num_results=100000,
                                                                       query_type=query_type_filtration,
