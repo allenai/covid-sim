8d4f6036f5afb24710e69df94938005008e77371
Shauli Ravfogel <shauli321@gmail.com>
2020-11-10 10:58:17 +0200

    Update alignment_supervised.py

--- api/covid-ai2/alignment_supervised.py
+++ api/covid-ai2/alignment_supervised.py
@@ -44,7 +44,7 @@ class BertModel(torch.nn.Module):
             self.tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
         
         if load_existing:
-            self.linear_arg1_1 = torch.load("Shauli/RE-metric-model-spike/linear.pt")
+            self.linear_arg1_1 = torch.load("linear.pt")
         else:
             self.linear_arg1_1 = torch.nn.Linear(768, 64)
         self.linear_arg2_1 = torch.nn.Linear(768, 64)
