d896dc4109a69faf670d8988cd7cb77d2c235916
Shauli Ravfogel <shauli321@gmail.com>
2020-11-16 10:57:23 +0200

    Update alignment_supervised.py

--- api/covid-ai2/alignment_supervised.py
+++ api/covid-ai2/alignment_supervised.py
@@ -19,7 +19,7 @@ from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from typing import Dict, Tuple
 from scipy.spatial.distance import cosine as cosine_distance
-from collections import defaultdict
+from collections import defaultdict, Counter
 import nltk
 from nltk import ngrams as get_ngrams
 from termcolor import colored
@@ -310,7 +310,8 @@ def evaluate_model(sents1, sents2, arg1_sent1, arg2_sent1, model, max_ngrams = 5
 
 def main(model, results_sents, spike_df, num_results, max_ngrams):
 
-
+    captures = []
+    
     def pretty_print(sent, idx_arg1, idx_arg2):
     
         sent_lst = sent.split(" ")
@@ -368,8 +369,15 @@ def main(model, results_sents, spike_df, num_results, max_ngrams):
         arg2_start = p["tok2orig"][ngram_pred_arg2_idx[0]]
         arg2_end = p["tok2orig"][ngram_pred_arg2_idx[1]]        
         sent = p["sent"]
+        sent_lst = sent.split(" ")
+        captures.append((sent_lst[arg1_start:arg1_end], sent_lst[arg2_start:arg2_end]))
        
         annotated_sent = perform_annotation(sent, [[arg1_start, arg1_end], [arg2_start, arg2_end]])
         annotated_sent = annotated_sent[p["l"]:]
         annotated.append(annotated_sent)
-    return annotated
+    
+    # aggregate arguments
+    args1, args2 = list(zip(*captures))
+    arg1_counter, arg2_counter = Counter(args1), Counter(args2)
+    
+    return annotated, list(arg1_counter.items()), list(arg2_counter.item()) 
