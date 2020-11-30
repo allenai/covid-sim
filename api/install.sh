cdc730f0a6e2b7db59bb188eb01787a2b8f29481
Shauli Ravfogel <shauli321@gmail.com>
2020-11-30 09:08:44 +0200

    Update install.sh

--- api/install.sh
+++ api/install.sh
@@ -3,6 +3,6 @@ pip install htbuilder
 pip install termcolor
 #conda install faiss-cpu -c pytorch
 pip install -Iv faiss-cpu==1.6.3
-conda install faiss-cpu -c pytorch
+conda install faiss-cpu=1.6.3 -c pytorch
 python3 download_bert_model.py
 
