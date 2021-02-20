4fda4ba35c361baf208b52c598a82d9e6363abee
Shauli Ravfogel <shauli321@gmail.com>
2021-02-20 22:22:27 +0200

    Update demo.py

--- api/covid-ai2/demo.py
+++ api/covid-ai2/demo.py
@@ -278,7 +278,7 @@ if (start or session_state.start) and session_state.started:
            encoding_projected = project_out(encoding_pos, encoding_neg)
        else:
            encoding_projected = encoding_pos
-       encoding = np.mean(encoding_projected, axis = 0).squeeze()
+       encoding = np.mean(encoding_projected, axis = 0)
 
        #encoding = encoding - encoding_neg
        session_state.enhance = set()
