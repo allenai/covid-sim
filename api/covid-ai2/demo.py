a5b3b4f7a49613547e99f128414111914bdf0741
Shauli Ravfogel <shauli321@gmail.com>
2021-03-17 22:51:55 +0200

    Update demo.py

--- api/covid-ai2/demo.py
+++ api/covid-ai2/demo.py
@@ -125,7 +125,7 @@ def get_table_download_link(df):
     in:  dataframe
     out: href string
     """
-    csv = df.to_csv(index=False)
+    csv = df.to_csv(index=False, sep = "\t")
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
     href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
     return href
